d386632f311c8a76dac6094207cb1f0129ddc982
Manoj-Katkar <satyammanoj2001@gmail.com>
2023-01-30 18:45:37 +0530

    all assignment till 29th jan is added

---
+++ practical.java
@@ -0,0 +1,30 @@
+public class practical {
+    public static void main(String[] args) {
+        // 1) Write a program to print capital Alphabets using pattern programming in
+        // java ?
+        /*
+         * for (int i = 65; i <= 90; i++) {
+         * System.out.print((char) i + " ");
+         * }
+         */
+
+        // 2) Write a program to print triangle sing star pattern programming logic in
+        // java ?
+        int rows = 5;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 3) Write a program to print PW SKILL using pattern programming logic in java
+        // ?
+        System.out.println("PW SKILL");
+
+        // 4)Write a program to print Your Full name using pattern programming logic in
+        // java ?
+        System.out.println("MANOJ VITTHAL KATKAR");
+
+    }
+}
