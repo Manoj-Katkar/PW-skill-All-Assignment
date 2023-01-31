6ce02bbba0a230f31716e883ca55608935d11070
Manoj Katkar <satyammanoj2001@gmail.com>
2023-01-31 15:37:31 +0530

    comments added

--- Ifelse_using_ternary_operater.java
+++ Ifelse_using_ternary_operater.java
@@ -3,7 +3,8 @@ public class Ifelse_using_ternary_operater {
         int a = 100;
         int b = 50;
 
-        boolean result = (a < b)? true: false;
+        boolean result = (a < b)? true: false;  //after the question will get executed if the condition is true else another false will get executed 
+                                                //This is get ask in the MCQ
         System.out.println(result);
 
     }
